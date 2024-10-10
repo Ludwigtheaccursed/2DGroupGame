7e5273541669243ee8cc34d9b8700c32304ada63
ToasterCat47 <Cooper@lyon-den.net>
2024-10-10 13:59:28 -0700

    Working on enemy ai

---
+++ Assets/Scritps/EnemyPatrolAI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolAI : MonoBehaviour
+{
+    [SerializeField]
+    float EnemySpeed = 5f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
