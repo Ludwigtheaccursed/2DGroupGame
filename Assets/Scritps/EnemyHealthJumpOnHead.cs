9c4e302336a73ba2a90744118beb15d1b6d65ff1
ToasterCat47 <Cooper@lyon-den.net>
2024-10-11 12:41:46 -0700

    Adding and fixing enemy ai

---
+++ Assets/Scritps/EnemyHealthJumpOnHead.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthJumpOnHead : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
