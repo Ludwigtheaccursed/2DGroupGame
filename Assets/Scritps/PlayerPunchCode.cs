a498fa8feafba8f1f8b5d6024d7cbb18fe11b87d
ToasterCat47 <Cooper@lyon-den.net>
2024-10-14 13:56:31 -0700

    Working on player punching

---
+++ Assets/Scritps/PlayerPunchCode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPunchCode : MonoBehaviour
+{
+    [SerializeField]
+    float SlapForce = 10f;
+    GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+        Vector3 playerPosition = Player.transform.position;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<SpriteRenderer>.OrderInLayer = 100;
+        collision.gameObject.GetComponent<BoxCollider2D>.IsTrigger = true;
+        Vector3 SlapDirection = collision.transform.position;
+
+        collision.gameObject.GetComponent<Rigidbody2D>().velocity = SlapDirection * SlapForce * -1;
+    }
+}
