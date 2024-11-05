aa6d8125f6ee11a18268a25609002ccbdca00f68
ToasterCat47 <Cooper@lyon-den.net>
2024-11-05 11:43:55 -0800

    Fixed Bug

--- Assets/Scritps/Void.cs
+++ Assets/Scritps/Void.cs
@@ -17,18 +17,22 @@ public class Void : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth3>() != null)
+        if (collision.tag == "Player")
         {
-         GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth3>().PlayerHealth = 0;
-         Debug.Log("DiedToVoid");
+            if (GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth3>() != null)
+            {
+                GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth3>().PlayerHealth = 0;
+                Debug.Log("DiedToVoid");
+            }
+            if (GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth2>() != null)
+            {
+                GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth2>().PlayerHealth = 0;
+                Debug.Log("DiedToVoid");
+            }
+            else
+            {
+                Debug.Log("eeee");
+            }
         }
-        if (GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth2>() != null)
-        {
-            GameObject.Find("PlayerHitBox").GetComponent<PlayerHealth2>().PlayerHealth = 0;
-            Debug.Log("DiedToVoid");
-        }
-        else {
-            Debug.Log("eeee"); 
-                }
     }
 }
