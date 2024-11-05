32074695d32403c4cfd07fea69845619f6f5dd1e
ToasterCat47 <Cooper@lyon-den.net>
2024-11-05 12:10:18 -0800

    Made Gnome PreFab

---
+++ Assets/Scritps/GnomeACS.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class GnomeACS : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rb;
+    BoxCollider2D bc;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        rb = gameObject.GetComponentInParent<Rigidbody2D>();
+        bc = gameObject.GetComponentInParent<BoxCollider2D>();
+    }
+    void Update()
+    {
+        if (rb.velocity.x > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            bc.offset.Equals(-1 * bc.offset) ;
+        }
+        if (rb.velocity.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            bc.offset.Equals(-1 * bc.offset);
+        }
+        if (gameObject.GetComponentInParent<EnemyDieSpinn>().enabled == true)
+        {
+            anim.SetBool("IsDead", true);
+        }
+    }
+}
